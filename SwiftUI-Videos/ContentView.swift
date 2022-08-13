3e69bd9de0d324fe122a6b291cd4cec25a311c9a
murad <muradkv@gmail.com>
2022-08-13 21:24:52 +0300

    Create List and NavigationView

--- SwiftUI-Videos/ContentView.swift
+++ SwiftUI-Videos/ContentView.swift
@@ -9,25 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            Image("90-90")
-                .resizable()
-                .scaledToFit()
-                .frame(height: 90.0)
-                .cornerRadius(8)
-            
-            Spacer().frame(width: 16)
-            
-            VStack(alignment: .leading) {
-                Text("The 90-90 Rule is a great rule")
-                    .fontWeight(.semibold)
-                    .lineLimit(2)
-                    .minimumScaleFactor(0.75)
-                Spacer().frame(height: 5)
-                Text("January 23, 2020")
-                    .font(.subheadline)
-                    .foregroundColor(.secondary)
-            }
+        NavigationView {
+            List(0..<20) { item in
+                HStack {
+                    Image("90-90")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 90.0)
+                        .cornerRadius(8)
+                    
+                    Spacer().frame(width: 16)
+                    
+                    VStack(alignment: .leading) {
+                        Text("The 90-90 Rule is a great rule")
+                            .fontWeight(.semibold)
+                            .lineLimit(2)
+                            .minimumScaleFactor(0.75)
+                        Spacer().frame(height: 5)
+                        Text("January 23, 2020")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }.navigationBarTitle(Text("Seans Videos"))
         }
     }
 }
