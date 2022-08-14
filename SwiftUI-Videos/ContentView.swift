59474ed984c1656d8fa46a307f8b6cf9f81088c9
murad <muradkv@gmail.com>
2022-08-14 19:21:11 +0300

    Show content to List from Video Model data

--- SwiftUI-Videos/ContentView.swift
+++ SwiftUI-Videos/ContentView.swift
@@ -8,11 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var videos: [Video] = []
+    
     var body: some View {
         NavigationView {
-            List(0..<20) { item in
+            List(videos) { video in
                 HStack {
-                    Image("90-90")
+                    Image(video.imageName)
                         .resizable()
                         .scaledToFit()
                         .frame(height: 90.0)
@@ -21,12 +24,12 @@ struct ContentView: View {
                     Spacer().frame(width: 16)
                     
                     VStack(alignment: .leading) {
-                        Text("The 90-90 Rule is a great rule")
+                        Text(video.title)
                             .fontWeight(.semibold)
                             .lineLimit(2)
                             .minimumScaleFactor(0.75)
                         Spacer().frame(height: 5)
-                        Text("January 23, 2020")
+                        Text(video.uploadDate)
                             .font(.subheadline)
                             .foregroundColor(.secondary)
                     }
@@ -38,6 +41,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(videos: VideoList.topTwelve)
     }
 }
